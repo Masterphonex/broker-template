9a88d0138489b6170b31fd94b9127d2d595d57c2
React <masterphonexpeboss@gmail.com>
2024-10-04 14:24:56 +0100

    updated login and register page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -9,9 +9,10 @@ export default function Login() {
     <div className='bg-slate-900 w-full flex flex-col gap-8 items-center justify-center h-[89vh] overflow-y-hidden text-white'>
       <div className='text-white flex gap-5 items-center'>
       <FaLock size={60}/>
-      <h1 className='text-3xl font-bold text-white'> </h1>
       </div>
-        <form className={` sm:w-[400px] w-[300px] flex flex-col gap-6 rounded-md shadow-sm items-center justify-center px-5 py-5 bg-slate-900 ${styles.container} shadow-slate-950`}>
+        <form className={` sm:w-[400px] w-[300px] flex flex-col gap-6 rounded-2xl shadow-sm items-center justify-center px-5 py-5 bg-slate-900 ${styles.container} shadow-slate-950`}>
+        <h1 className='text-2xl my-4 font-bold text-white'> Login</h1>
+
         <input name=" email" type="email" placeholder="Email Address" className="input input-bordered w-full max-w-xs bg-transparent shadow-lg  border-slate-700 text-white font-bold " />
         <input name="password" type="password"  placeholder="Password" className="input input-bordered w-full max-w-xs bg-transparent shadow-lg border border-slate-700  text-white font-bold" />
        <Link href="/dashboard"> <button className="btn btn-primary w-[120px] fonr-bold text-lg">Login</button></Link>
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -6,12 +6,13 @@ import Link from 'next/link';
 
 export default function Register() {
   return (
-    <div className='bg-slate-900 w-full flex flex-col gap-8 items-center justify-center h-[89vh] overflow-y-hidden text-white'>
+    <div className='bg-slate-900 w-full flex flex-col gap-8 items-center justify-center   text-white'>
     <div className='text-white flex gap-5 items-center'>
     <FaDoorOpen size={60}/>
-    <h1 className='text-3xl font-bold text-white'> </h1>
     </div>
-      <form className={` sm:w-[400px] w-[300px] flex flex-col gap-6 rounded-md shadow-sm items-center justify-center px-5 py-5 bg-slate-900 ${styles.container} shadow-slate-950`}>
+      <form className={` sm:w-[400px] w-[300px] flex flex-col mb-4 gap-6 rounded-2xl shadow-sm items-center justify-center px-5 py-5 bg-slate-900 ${styles.container} shadow-slate-950`}>
+    <h1 className='text-3xl font-bold text-white my-4'>Register</h1>
+
       <input name="fullname" type="text" placeholder="Fullname" className="input input-bordered w-full max-w-xs bg-transparent shadow-lg  border-slate-700 text-white font-bold " />
       <input name="username" type="text" placeholder="username" className="input input-bordered w-full max-w-xs bg-transparent shadow-lg  border-slate-700 text-white font-bold " />
       <input name=" email" type="email" placeholder="Email Address" className="input input-bordered w-full max-w-xs bg-transparent shadow-lg  border-slate-700 text-white font-bold " />
